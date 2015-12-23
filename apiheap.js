1c4e149384c6578a5e8ea948e3ba9c7bbe5f5c2d
tash-had <nanylagoon@gmail.com>
2015-12-22 22:49:08 -0500

    finished bitly link shortening

--- apiheap.js
+++ apiheap.js
@@ -1,34 +1,62 @@
-var SOURCE_ID; 
-var SOURCE_NAME; 
-var SOURCE_KEY; 
-var SOURCE_REQ_ITEM; 
-var RETURN_VAL = []; 
+var SHORT_URL = null; 
 
-function apiheap1(source, name, key, reqItem){
-}
 
 function apiheap(source){
 	SOURCE_ID = source.toLowerCase(); 
 	if(SOURCE_ID === "reddit"){
-		this.reddit = function(subreddit, reqItem){
+		this.reddit = function(subreddit, params, reqItem){
+			this.SOURCE_NAME = subreddit; 
+			this.FINAL_URL = "http://www.reddit.com/r/" + this.SOURCE_NAME; 
 
 		}
 
 	}else if(SOURCE_ID==="tumblr"){
 		this.tumblr = function(tumblr_url, key, reqItem){
-
+			this.SOURCE_NAME = tumblr_url; 
+			this.SOURCE_KEY = key; 
 		}
 
 	}else if(SOURCE_ID==="bitly"){
 		this.bitly = function(key, link){
-			
+			this.SOURCE_NAME = link; 
+			this.SOURCE_KEY = key; 
+			var bitly_frame = "https://api-ssl.bitly.com/v3/shorten?access_token=" + this.SOURCE_KEY + "&"; 
+			this.FINAL_URL = bitly_frame + "longUrl=" + encodeURIComponent(this.SOURCE_NAME) + "&format=json"; 
+			try{
+				$.ajax({
+					url: this.FINAL_URL, 
+					dataType: 'json', 
+					type: 'GET', 
+					cache: false, 
+					success:function(data){
+						$(data.data).each(function(index, value){
+							SHORT_URL = "http://bit.ly/" + value.hash;
+						});
+					}
+				});
+			}catch(err){
+				errorHandle("bitly request error");
+			}
+
 		}
 	}else{
-		return "APIHEAP SOURCE ERROR."; 
+		errorHandle("invalid source");  
 	}
 }
+function errorHandle(errMessage){
+	alert("APIHEAP ERROR: "+ errMessage); 
+}
 
-var x = new apiheap("reddit");
-//document.write(x.apiheapReq("yee")); 
 
+/*
+** BITLY https://bitly.com/a/oauth_apps 
+PARAMS: oathkey, longlink
 
+var myVar = new apiheap("bitly"); 
+myVar.bitly("auth_token", "long_link");
+function onClickMe(){
+	document.write(SHORT_URL); 
+} 
+
+*NOTE: CALL AFTER AN ACTION EG.BUTTON PRESS. OTHERWISE VALUE = null. 
+*/
