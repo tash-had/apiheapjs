fb81b6486dfb7b0499763d890c2342dbbaaccc02
tash-had <nanylagoon@gmail.com>
2015-12-29 22:26:14 -0500

    Updated parsing process, minimized code

--- apiheap.js
+++ apiheap.js
@@ -42,15 +42,24 @@ function apiheap(source, key) {
                     }
                 }
             }
+            this.RESPONSE = apiRequest('json', this.FINAL_URL);
+        }
+        this.parse = function(item) {
+            var RETURN_VALUE = [],
+                reddit_json_data = this.RESPONSE,
+                itemReq = item;
             try {
-                this.RESPONSE = $.ajax({
-                    url: this.FINAL_URL,
-                    dataType: 'json',
-                    type: 'GET',
-                    cache: false
-                });
+                if (isUndefined(itemReq)) {
+                    RETURN_VALUE = reddit_json_data.responseJSON.data.children;
+                } else {
+                    $(reddit_json_data.responseJSON.data.children).each(function(index, value) {
+                        RETURN_VALUE.push(value.data[itemReq]);
+                    });
+                }
             } catch (err) {
-                errorHandle("error| LOG:" + err + " ..." + "reddit request error");
+                errorHandle("error| LOG:" + err + " ..." + "reddiit parse error");
+            } finally {
+                return RETURN_VALUE;
             }
         }
     } else if (SOURCE_ID === "tumblr") {
@@ -74,15 +83,34 @@ function apiheap(source, key) {
                     this.FINAL_URL = tumblr_frame + "/posts/" + this.REQ_ITEM + "?api_key=" + this.SOURCE_KEY + "&" + this.OPT_PARAMS;
                 }
             }
-            try {
-                this.RESPONSE = $.ajax({
-                    url: this.FINAL_URL,
-                    type: 'GET',
-                    dataType: 'jsonp',
-                    cache: false
-                });
-            } catch (err) {
-                errorHandle("error| LOG:" + err + " ..." + "tumblr request error");
+            this.RESPONSE = apiRequest('jsonp', this.FINAL_URL);
+        }
+        this.parse = function(item) {
+            var tumblrResponse = this.RESPONSE.responseJSON.response,
+                itemReq = item;
+            if (isUndefined(itemReq)) {
+                return tumblrResponse.posts;
+            } else {
+                if (itemReq === "blog_info") {
+                    return tumblrResponse.blog;
+                } else if (itemReq === "total_posts") {
+                    return tumblrResponse.total_posts;
+                } else {
+                    var RETURN_VALUE = [];
+                    try {
+                        $(tumblrResponse.posts).each(function(index, value) {
+                            if (itemReq === "photos_url") {
+                                $(value).each(function(idx, val) {
+                                    RETURN_VALUE.push(val.photos[0].original_size.url);
+                                });
+                            } else {
+                                RETURN_VALUE.push(value[itemReq]);
+                            }
+                        });
+                    } catch (err) {} finally {
+                        return RETURN_VALUE;
+                    }
+                }
             }
         }
     } else if (SOURCE_ID === "bitly") {
@@ -91,61 +119,86 @@ function apiheap(source, key) {
         this.bitly = function(link) {
             this.SOURCE_NAME = link;
             this.FINAL_URL = bitly_frame + "longUrl=" + encodeURIComponent(this.SOURCE_NAME) + "&format=json";
-            try {
-                this.RESPONSE = $.ajax({
-                    url: this.FINAL_URL,
-                    dataType: 'json',
-                    type: 'GET',
-                    cache: false
-                });
-
-            } catch (err) {
-                errorHandle("error| LOG:" + err + " ..." + "bitly request error");
-            }
+            this.RESPONSE = apiRequest('json', this.FINAL_URL);
+        }
+        this.parse = function() {
+            var bitly_response = JSON.parse(this.RESPONSE.responseText);
+            return bitly_response.data.url;
         }
     } else if (SOURCE_ID === "imgur") {
         this.SOURCE_KEY = key;
         this.imgur = function(tag_name, opt_params, topic) {
             this.TAG = tag_name;
             this.OPT_PARAMS = opt_params;
+            this.TOPIC = topic;
 
-            if (topic) {
-                if (isUndefined(this.TAG)) {
-                    errorHandle("error| LOG: imgur request error: you need to set a topic name");
-                } else {
+            if (isUndefined(tag_name) && isUndefined(opt_params) && isUndefined(topic)) {
+                this.TOPIC = true;
+            } else {
+                if (topic) {
                     if (isUndefined(this.OPT_PARAMS)) {
                         this.FINAL_URL = "https://api.imgur.com/3/topics/" + this.TAG;
                     } else {
                         this.FINAL_URL = "https://api.imgur.com/3/topics/" + this.TAG + "/" + this.OPT_PARAMS;
                     }
-                }
-            } else {
-                if (isUndefined(this.TAG)) {
-                    if (isUndefined(this.OPT_PARAMS)) {
-                        this.FINAL_URL = "https://api.imgur.com/3/gallery/";
-                    } else {
-                        this.FINAL_URL = "https://api.imgur.com/3/gallery/" + this.OPT_PARAMS;
-                    }
                 } else {
-                    if (isUndefined(this.OPT_PARAMS)) {
-                        this.FINAL_URL = "https://api.imgur.com/3/gallery/t/" + this.TAG;
+                    if (isUndefined(this.TAG)) {
+                        if (isUndefined(this.OPT_PARAMS)) {
+                            this.FINAL_URL = "https://api.imgur.com/3/gallery/";
+                        } else {
+                            this.FINAL_URL = "https://api.imgur.com/3/gallery/" + this.OPT_PARAMS;
+                        }
                     } else {
-                        this.FINAL_URL = "https://api.imgur.com/3/gallery/t/" + this.TAG + "/" + this.OPT_PARAMS;
+                        if (isUndefined(this.OPT_PARAMS)) {
+                            this.FINAL_URL = "https://api.imgur.com/3/gallery/t/" + this.TAG;
+                        } else {
+                            this.FINAL_URL = "https://api.imgur.com/3/gallery/t/" + this.TAG + "/" + this.OPT_PARAMS;
+                        }
                     }
+                }
+                try {
+                    this.RESPONSE = $.ajax({
+                        url: this.FINAL_URL,
+                        dataType: 'json',
+                        type: 'GET',
+                        cache: false,
+                        headers: {
+                            "Authorization": "Client-ID " + this.SOURCE_KEY
+                        }
+                    });
+                } catch (err) {
+                    errorHandle("error| LOG:" + err + " ..." + "imgur request error");
                 }
             }
-            try {
-                this.RESPONSE = $.ajax({
-                    url: this.FINAL_URL,
-                    dataType: 'json',
-                    type: 'GET',
-                    cache: false,
-                    headers: {
-                        "Authorization": "Client-ID " + this.SOURCE_KEY
+        }
+        this.parse = function(itemReq) {
+            var RETURN_VALUE = [],
+                topic = this.TOPIC,
+                imgur_api_response = this.RESPONSE;
+            if (isUndefined(itemReq)) {
+                if (!topic || isUndefined(topic)) {
+                    return imgur_api_response.responseJSON.data.items;
+                } else {
+                    return imgur_api_response.responseJSON.data;
+                }
+            } else {
+                try {
+                    if (!topic || isUndefined(topic)) {
+                        $(imgur_api_response.responseJSON.data.items).each(function(index, value) {
+                            $(value).each(function(idx, val) {
+                                RETURN_VALUE.push(val[itemReq]);
+                            });
+                        });
+                    } else {
+                        $(imgur_api_response.responseJSON.data).each(function(index, value) {
+                            $(value).each(function(idx, val) {
+                                RETURN_VALUE.push(val[itemReq]);
+                            });
+                        });
                     }
-                });
-            } catch (err) {
-                errorHandle("error| LOG:" + err + " ..." + "imgur request error");
+                } catch (err) {} finally {
+                    return RETURN_VALUE;
+                }
             }
         }
     } else if (SOURCE_ID === "youtube") {
@@ -175,15 +228,66 @@ function apiheap(source, key) {
                     }
                 }
             }
-            try {
-                this.RESPONSE = $.ajax({
-                    url: this.FINAL_URL,
-                    dataType: 'json',
-                    type: 'GET',
-                    cache: false
-                });
-            } catch (err) {
-                errorHandle("error| LOG:" + err + " ..." + "youtube request error");
+            this.RESPONSE = apiRequest('json', this.FINAL_URL);
+        }
+        this.parse = function(query_array, item) {
+            if (isUndefined(item)) {
+                errorHandle("youtube parse error: you must request an item as a parameter");
+            } else {
+                var youtube_data_array = query_array,
+                    itemReq = item,
+                    RETURN_VALUE = [];
+                try {
+                    $(youtube_data_array).each(function(idx, val) {
+                        $(val.responseJSON.items).each(function(index, value) {
+                            switch (itemReq) {
+                                case "type":
+                                    RETURN_VALUE.push(value.id.kind);
+                                    break;
+
+                                case "vid_links":
+                                    RETURN_VALUE.push("https://www.youtube.com/watch?v=" + value.id.videoId);
+                                    break;
+
+                                case "vid_ids":
+                                    RETURN_VALUE.push(value.id.videoId);
+                                    break;
+
+                                case "vid_dates":
+                                    RETURN_VALUE.push(value.snippet.publishedAt);
+                                    break;
+
+                                case "vid_titles":
+                                    RETURN_VALUE.push(value.snippet.title);
+                                    break;
+
+                                case "vid_thumbnails":
+                                    RETURN_VALUE.push(value.snippet.thumbnails.high.url);
+                                    break;
+
+                                case "chan_links":
+                                    RETURN_VALUE.push("https://www.youtube.com/channel/" + value.snippet.channelId);
+                                    break;
+
+                                case "chan_ids":
+                                    RETURN_VALUE.push(value.snippet.channelId);
+                                    break;
+
+                                case "chan_names":
+                                    RETURN_VALUE.push(value.snippet.channelTitle);
+                                    break;
+
+                                case "vid_descriptions":
+                                    RETURN_VALUE.push(value.snippet.description);
+                                    break;
+                            }
+                        });
+                    });
+                } catch (err) {
+                    errorHandle("youtube parse error");
+                } finally {
+                    return RETURN_VALUE;
+                }
             }
         }
     } else if (SOURCE_ID === "openweathermap") {
@@ -201,15 +305,20 @@ function apiheap(source, key) {
             } else {
                 this.FINAL_URL = "http://api.openweathermap.org/data/2.5/weather?" + this.CUSTOM + "&appid=" + this.SOURCE_KEY;
             }
-            try {
-                this.RESPONSE = $.ajax({
-                    url: this.FINAL_URL,
-                    dataType: 'json',
-                    type: 'GET',
-                    cache: false
-                });
-            } catch (err) {
-                errorHandle("openweathermap request error. LOG| " + err);
+            this.RESPONSE = apiRequest('json', this.FINAL_URL);
+        }
+        this.parse = function(item) {
+            var weather_json_data = this.RESPONSE,
+                itemReq = item;
+
+            if (isUndefined(itemReq)) {
+                return weather_json_data.responseJSON;
+            } else {
+                if (itemReq === "weather") {
+                    return weather_json_data.responseJSON[itemReq][0].description;
+                } else {
+                    return weather_json_data.responseJSON[itemReq];
+                }
             }
         }
 
@@ -218,162 +327,19 @@ function apiheap(source, key) {
     }
 }
 
-function bitlyParse(bitly_json_data) {
-    var bitly_response = JSON.parse(bitly_json_data.responseText);
-    return bitly_response.data.url;
-}
-
-function tumblrParse(tumblr_json_data, item) {
-    var tumblrResponse = tumblr_json_data.responseJSON.response,
-        itemReq = item;
-    if (isUndefined(itemReq)) {
-        return tumblrResponse.posts;
-    } else {
-        if (itemReq === "blog_info") {
-            return tumblrResponse.blog;
-        } else if (itemReq === "total_posts") {
-            return tumblrResponse.total_posts;
-        } else {
-            var RETURN_VALUE = [];
-            try {
-                $(tumblrResponse.posts).each(function(index, value) {
-                    if (itemReq === "photos_url") {
-                        $(value).each(function(idx, val) {
-                            RETURN_VALUE.push(val.photos[0].original_size.url);
-                        });
-                    } else {
-                        RETURN_VALUE.push(value[itemReq]);
-                    }
-                });
-            } catch (err) {} finally {
-                return RETURN_VALUE;
-            }
-        }
-    }
-}
-
-function imgurParse(json_data, itemReq, topic) {
-    var RETURN_VALUE = [],
-        imgur_api_response = json_data;
-    if (isUndefined(itemReq)) {
-        if (!topic || isUndefined(topic)) {
-            return imgur_api_response.responseJSON.data.items;
-        } else {
-            return imgur_api_response.responseJSON.data;
-        }
-    } else {
-        try {
-            if (!topic || isUndefined(topic)) {
-                $(imgur_api_response.responseJSON.data.items).each(function(index, value) {
-                    $(value).each(function(idx, val) {
-                        RETURN_VALUE.push(val[itemReq]);
-                    });
-                });
-            } else {
-                $(imgur_api_response.responseJSON.data).each(function(index, value) {
-                    $(value).each(function(idx, val) {
-                        RETURN_VALUE.push(val[itemReq]);
-                    });
-                });
-            }
-        } catch (err) {} finally {
-            return RETURN_VALUE;
-        }
-    }
-}
-
-function redditParse(json_data, item) {
-    var RETURN_VALUE = [],
-        reddit_json_data = json_data,
-        itemReq = item;
+function apiRequest(format, FINAL_URL) {
+    var response;
     try {
-        if (isUndefined(itemReq)) {
-            RETURN_VALUE = reddit_json_data.responseJSON.data.children;
-        } else {
-            $(reddit_json_data.responseJSON.data.children).each(function(index, value) {
-                RETURN_VALUE.push(value.data[itemReq]);
-            });
-        }
+        response = $.ajax({
+            url: FINAL_URL,
+            dataType: format,
+            type: 'GET',
+            cache: false
+        });
     } catch (err) {
-        errorHandle("error| LOG:" + err + " ..." + "reddiit parse error");
+        errorHandle("Request Error. Log| " + err);
     } finally {
-        return RETURN_VALUE;
-    }
-}
-
-function youtubeParse(query_array, item) {
-    if (isUndefined(item)) {
-        errorHandle("youtube parse error: you must request an item as a parameter");
-    } else {
-        var youtube_data_array = query_array,
-            itemReq = item,
-            RETURN_VALUE = [];
-        try {
-            $(youtube_data_array).each(function(idx, val) {
-                $(val.responseJSON.items).each(function(index, value) {
-                    switch (itemReq) {
-                        case "type":
-                            RETURN_VALUE.push(value.id.kind);
-                            break;
-
-                        case "vid_links":
-                            RETURN_VALUE.push("https://www.youtube.com/watch?v=" + value.id.videoId);
-                            break;
-
-                        case "vid_ids":
-                            RETURN_VALUE.push(value.id.videoId);
-                            break;
-
-                        case "vid_dates":
-                            RETURN_VALUE.push(value.snippet.publishedAt);
-                            break;
-
-                        case "vid_titles":
-                            RETURN_VALUE.push(value.snippet.title);
-                            break;
-
-                        case "vid_thumbnails":
-                            RETURN_VALUE.push(value.snippet.thumbnails.high.url);
-                            break;
-
-                        case "chan_links":
-                            RETURN_VALUE.push("https://www.youtube.com/channel/" + value.snippet.channelId);
-                            break;
-
-                        case "chan_ids":
-                            RETURN_VALUE.push(value.snippet.channelId);
-                            break;
-
-                        case "chan_names":
-                            RETURN_VALUE.push(value.snippet.channelTitle);
-                            break;
-
-                        case "vid_descriptions":
-                            RETURN_VALUE.push(value.snippet.description);
-                            break;
-                    }
-                });
-            });
-        } catch (err) {
-            errorHandle("youtube parse error");
-        } finally {
-            return RETURN_VALUE;
-        }
-    }
-}
-
-function weatherParse(json_data, item) {
-    var weather_json_data = json_data,
-        itemReq = item;
-
-    if (isUndefined(itemReq)) {
-        return weather_json_data.responseJSON;
-    } else {
-        if (itemReq === "weather") {
-            return weather_json_data.responseJSON[itemReq][0].description;
-        } else {
-            return weather_json_data.responseJSON[itemReq];
-        }
+        return response;
     }
 }
 
